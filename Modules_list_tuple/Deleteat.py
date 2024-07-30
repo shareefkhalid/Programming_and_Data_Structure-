73d298b8deb88f383376dcbec7a3ad7c26f28b2f
shareefkhalid <shareefkhaild005@gmail.com>
2024-07-30 08:51:48 +0200

    Delete at.py

---
+++ Modules_list_tuple/Deleteat.py
@@ -0,0 +1,9 @@
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    return my_list[:idx] + my_list[idx+1:]
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)  
+print(my_list)   
