213ed9e72e0ce80aa536306ed84208a7cbce4b6d
shareefkhalid <shareefkhaild005@gmail.com>
2024-11-09 14:54:15 -0800

    0-square.py

---
+++ classes/0-square.py
@@ -0,0 +1,16 @@
+class A:
+    def printA(self):
+        print('classA')
+class B:
+    def printB(self):
+        print('classB')
+
+class c:
+    def classC(self):
+        print('classc')
+
+objA= A()
+objA.printA()
+
+objB= B()
+objB.printB()
